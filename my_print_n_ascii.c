c10a8e895b2ea57c3c4615d16fc72dac8068f209
kanoise <139328330+kanoise@users.noreply.github.com>
2023-07-12 11:16:12 +0000

    my_print_n_ascii

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -4,12 +4,10 @@ void my_print_n_ascii(int howMany)
 {
     int a = howMany;
 
+    if (a <= 94)
+    {
     for (char i = 0; i < a; i++) {
         print_char(i + 33); 
     }
-
-    if (a > 33)
-    {
-        printf("")
     }
 }
